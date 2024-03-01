09ce25a499b2a55bd929a2dd6ce377a165c71538
Gustavo Cruz <106942151+G2309@users.noreply.github.com>
2024-03-01 15:54:15 -0600

    Final script.js
    Se terminó el chat, faltó implementar el preview de urls que no fueran imágenes

--- script.js
+++ script.js
@@ -1,73 +1,277 @@
+//
+//
 // 			Gustavo Cruz 22779
 //
 //
-var cuadrado4 = document.getElementById('cuadrado1');
-var cuadrado4 = document.getElementById('cuadrado2');
-var cuadrado4 = document.getElementById('cuadrado3');
-var cuadrado4 = document.getElementById('cuadrado4');
 
+let isDarkMode = false;
+let cuadrado1, cuadrado2, cuadrado3, cuadrado4;
+let displayedUsers = new Set();
 
-function toggleModo() {
-    var body = document.querySelector('body');
+function toggleMode() {
+  const boxes = document.querySelectorAll('.box');
+  isDarkMode = !isDarkMode;
 
-    var colorActual = body.style.backgroundColor;
+  if (isDarkMode) {
+    document.body.style.backgroundColor = '#333';
+    button.style.backgroundColor= '#fff';
+    button.style.color= '#333';
+    boxes.forEach(box => {
+      box.style.backgroundColor = '#fff'; 
+    });
+  } else {
+    document.body.style.backgroundColor = '#fff'; 
+    button.style.backgroundColor= '#333';
+    button.style.color= '#fff';
+    boxes.forEach(box => {
+      box.style.backgroundColor = '#fff'; 
+    });
+  }
+}
 
-    if (colorActual === '' || colorActual === 'white') {
-        body.style.backgroundColor = 'black';
-        body.style.color = 'white'; 
-    } else {
-        body.style.backgroundColor = 'white';
-        body.style.color = 'black'; 
+// Función dada en la tarea
+function validarURL(str) {
+	const patron = new RegExp("^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&'\\(\\)\\*\\+,;=.]+$");
+	return patron.test(str);
+}
+
+// Función para renderizar la url que sea válida
+
+function renderizarURL(str){
+        const rend = document.createElement('div');
+        const imageformat = /\.(jpeg|gif|png|jpg)$/i.test(str);
+        if (imageformat){
+                const image = document.createElement('img');
+                image.src = str;
+                rend.appendChild(image);
+        } else{
+                const enlace = document.createElement('a');
+                enlace.href = str;
+                enlace.textContent = str;
+                rend.appendChild(enlace);
+        }
+        return rend;
+}
+
+async function loadUser() {
+	const response = await fetch('http://uwu-guate.site:3000/messages');
+	const users = await response.json();
+	users.forEach(user => {
+		if (!displayedUsers.has(user.username)) {
+			const userDiv = document.createElement('div');
+			userDiv.textContent = user.username;
+			userbox.appendChild(userDiv);
+			displayedUsers.add(user.username);
+		}
+	});
+}
+loadUser();
+
+// Funcion para cargar los mensajes de la api, también usa la funcion para renderizar url en caso de haber en el mensaje
+async function loadMessages() {
+    const response = await fetch('http://uwu-guate.site:3000/messages', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const messages = await response.json();
+    message_box.innerHTML = '';
+    
+    messages.forEach(message => {
+        if (message.content) {
+            const delMessage = message.content.length > 140 ? message.content.substring(0, 140) + '...' : message.content;
+            const messageE = document.createElement('div');
+            messageE.textContent = `${message.username}: ${delMessage}`;
+            
+            if(validarURL(message.content)){
+                const rend = renderizarURL(message.content);
+                messageE.appendChild(rend);
+            } 
+            message_box.appendChild(messageE);
+        }
+    });
+}
+
+// llamo a la función para que cargue los mensajes nada más se entra al chat y no tenga que esperar los 6seg de intervalo que puse
+loadMessages();
+
+async function sendMessage() {
+	const messageText = textBox.value;
+	const username = "Gustavo";
+	const messageObj = {"username": username, "message": messageText};  // Aquí descubrí que el mensaje: undefined se debía a que usaba content, por que así estaba en la api >:\
+	const response = await fetch('http://uwu-guate.site:3000/messages', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+	  	},
+        body: JSON.stringify(messageObj)
+    });
+
+	if(response.ok){
+		textBox.value='';
+		await loadMessages();
+	} else {
+        console.error('Error al enviar mensaje',response.statusText);
     }
 }
 
-function crearCuadrados() {
-    var body = document.querySelector('body');
-    // Cuadrado 1 será el cuadrado de los users
-    var cuadrado1 = document.createElement('div');
-    cuadrado1.classList.add('cuadrado');
-    cuadrado1.setAttribute('id', 'cuadrado1');
-    body.appendChild(cuadrado1);
-    // Cuadrado 2 será el cuadrado de el chat
-    var cuadrado2 = document.createElement('div');
-    cuadrado2.classList.add('cuadrado');
-    cuadrado2.setAttribute('id', 'cuadrado2');
-    body.appendChild(cuadrado2);
-    // Cuadrado 3 será el cuadrado del perfil de usuario
-    var cuadrado3 = document.createElement('div');
-    cuadrado3.classList.add('cuadrado');
-    cuadrado3.setAttribute('id', 'cuadrado3');
-    body.appendChild(cuadrado3);
-    // Cuadrado 4 sera el cuadro de input
-    var cuadrado4 = document.createElement('div');
-    cuadrado4.classList.add('cuadrado');
-    cuadrado4.setAttribute('id', 'cuadrado4');
-    body.appendChild(cuadrado4);
 
-    body.style.display = 'flex';
-    body.style.flexWrap = 'wrap';
-    body.style.height = '100vh'; 
-}
+setInterval(loadMessages, 6000);  // Esperará 6 seg para actualizar los mensajes
+setInterval(loadUser, 6000);  // Esperará 6 seg para actualizar los usuarios en caso que entre uno nuevo
 
-function modificarTamanosCuadrados() {
-    var cuadrado1 = document.getElementById('cuadrado1');
-    cuadrado1.style.width = 'calc(20% - 5px)';
-    cuadrado1.style.height = 'calc(90vh - 5px)';
+function filterM() {
+    const Smessage = messageSearch.value.toLowerCase();
+    const messages = message_box.querySelectorAll('div');
 
-    var cuadrado2 = document.getElementById('cuadrado2');
-    cuadrado2.style.width = 'calc(80% - 5px)'; 
-    cuadrado2.style.height = 'calc(90vh - 5px)';
-
-    var cuadrado3 = document.getElementById('cuadrado3');
-    cuadrado3.style.width = 'calc(40% - 5px)';
-    cuadrado3.style.height = 'calc(10vh - 5px)'; 
-
-    var cuadrado4 = document.getElementById('cuadrado4');
-    cuadrado4.style.width = 'calc(60% - 5px)'; 
-    cuadrado4.style.height = 'calc(10vh - 5px)';
+    messages.forEach(message => {
+        const messageText = message.textContent.toLowerCase();
+        if (messageText.includes(Smessage)) {
+            message.style.display = 'block';
+        } else {
+            message.style.display = 'none';
+        }
+    });
 }
 
 
-crearCuadrados();
-modificarTamanosCuadrados();
-toggleModo();
+// Flex
+const flexContainer=document.createElement('div');
+flexContainer.style.display='flex';
+flexContainer.style.flexWrap='wrap';
+document.body.appendChild(flexContainer);
+
+// Cuadrados
+for (let i = 1; i <= 4; i++) {
+	const cuadrado = document.createElement("div");
+	cuadrado.classList.add("cuadrado");
+	cuadrado.id='cuadrado${i}'
+	switch (i) {
+		case 1:
+			cuadrado.style.width = 'calc(20% - 3px)';
+			cuadrado.style.height = 'calc(90vh - 3px)';
+			cuadrado1=cuadrado;
+			break;
+		case 2:
+			cuadrado.style.width = 'calc(80% - 3px)';
+			cuadrado.style.height = 'calc(90vh - 3px)';
+			cuadrado2=cuadrado;
+			break;
+		case 3:
+			cuadrado.style.width = 'calc(20% - 3px)';
+			cuadrado.style.height = 'calc(10vh - 3px)';
+			cuadrado3=cuadrado;
+			break;
+		case 4:
+			cuadrado.style.width = 'calc(80% - 3px)';
+			cuadrado.style.height = 'calc(10vh - 3px)';
+			cuadrado4=cuadrado;
+			break;
+	}
+	flexContainer.appendChild(cuadrado);
+}
+
+const cuadrados = document.querySelectorAll('.cuadrado');
+cuadrados.forEach(cuadrado => {
+    cuadrado.style.border = '1px solid #666';
+});
+// Cuadrados config 
+cuadrado1.style.display='flex';
+cuadrado1.style.flexDirection='column-reverse'
+
+cuadrado2.style.display='flex';
+cuadrado2.style.flexWrap='wrap';
+cuadrado2.style.justifyContent='center';
+
+cuadrado3.style.display='flex';
+cuadrado3.style.flexWrap='wrap';
+cuadrado3.style.justifyContent='center';
+cuadrado3.style.alignItems='center';
+
+cuadrado4.style.display='flex';
+cuadrado4.style.flexWrap='wrap';
+cuadrado4.style.alignItems='center';
+cuadrado4.style.justifyContent='center';
+
+// Main  -- llame así a la parte donde creo los parametros de cada espacio del chat
+const button = document.createElement('button');
+button.textContent = 'Cambiar tema';
+button.onclick = toggleMode;
+Object.assign(button.style, {
+    border: 'none',
+    backgroundColor: '#333',
+    color: '#fff'
+});
+cuadrado2.appendChild(button);
+
+const message_box = document.createElement('div');
+Object.assign(message_box.style, {
+    width: '100%',
+    height: 'calc(85vh - 10px)', 
+    display: 'flex',
+    flexDirection: 'column',
+    overflowY: 'auto',
+    overflowX: 'hidden',   //Escondí en X por que el scroll puede estorbar en algunos mensajes, de esta forma se ve mejor
+    position: 'relative'
+});
+cuadrado2.appendChild(message_box);
+
+const userbox = document.createElement('div');
+Object.assign(userbox.style, {
+    width: '100%',
+    height: '85vh',
+    display: 'flex',
+    flexDirection: 'column',
+    overflowY: 'visible',
+    overflowX: 'hidden'
+});
+cuadrado1.appendChild(userbox);
+
+const usersd = document.createElement('div');
+Object.assign(usersd.style, {
+    width: '100%',
+    height: 'auto',
+    textAlign: 'center'
+});
+usersd.textContent = 'Listado de usuarios:';
+cuadrado1.appendChild(usersd);
+
+const textBoxStyles = {
+    width: 'calc(90% - 80px)',
+    height: '5vh'
+};
+const sendButtonStyles = {
+    height: '5vh',
+    width: '70px'
+};
+const messageSearchStyles = {
+    width: '90%',
+    height: '3vh'
+};
+
+const textBox = document.createElement('input');
+textBox.placeholder = 'Escribe tu mensaje: ';
+Object.assign(textBox.style, textBoxStyles);
+cuadrado4.appendChild(textBox);
+
+const sendButton = document.createElement('button');
+sendButton.textContent = 'Enviar';
+sendButton.onclick = sendMessage;  
+Object.assign(sendButton.style, sendButtonStyles);
+cuadrado4.appendChild(sendButton);
+
+const messageSearch = document.createElement('input');
+messageSearch.placeholder = 'Buscar mensaje: ';
+Object.assign(messageSearch.style, messageSearchStyles);
+cuadrado3.appendChild(messageSearch);
+
+const filterButton = document.createElement('button');
+filterButton.textContent = 'Buscar';
+filterButton.onclick = filterM;
+
+Object.assign(filterButton.style, {
+	height: '3vh',
+	width: '50px'
+});
+cuadrado3.appendChild(filterButton);
